769a56eec07e93a6539f7ced4c15878367efab69
Devaarth <devaarth@gmail.com>
2020-07-29 16:39:29 +0530

    Update script.js
    Moving tiles with colour and number

--- script.js
+++ script.js
@@ -2,55 +2,113 @@
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9;
 
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener("DOMContentLoaded", function () {
+
+    tile11 = {
+        id: 1,
+        color: '#6F98A8'
+    }
+    tile22 = {
+        id: 2,
+        color: '#2B8EAD'
+    }
+    tile33 = {
+        id: 3,
+        color: '#2F454E'
+    }
+    tile44 = {
+        id: 4,
+        color: '#2B8EAD'
+    }
+    tile55 = {
+        id: 5,
+        color: '#2F454E'
+    }
+    tile66 = {
+        id: 6,
+        color: '#BFBFBF'
+    }
+    tile77 = {
+        id: 7,
+        color: '#EFEFEF'
+    }
+    tile88 = {
+        id: 8,
+        color: '#6F98A8'
+    }
+    tile99 = {
+        id: 9,
+        color: '#2F454E'
+    }
     tile1 = document.getElementById('number1')
     tile2 = document.getElementById('number2')
     tile3 = document.getElementById('number3')
     tile4 = document.getElementById('number4')
     tile5 = document.getElementById('number5')
     tile6 = document.getElementById('number6')
-    tile7 = document.getElementById('number7');
+    tile7 = document.getElementById('number7')
     tile8 = document.getElementById('number8')
     tile9 = document.getElementById('number9')
+    container1 = document.getElementById('container1')
+    container2 = document.getElementById('container2')
+    container3 = document.getElementById('container3')
+    container4 = document.getElementById('container4')
+    container5 = document.getElementById('container5')
+    container6 = document.getElementById('container6')
+    container7 = document.getElementById('container7')
+    container8 = document.getElementById('container8')
+    container9 = document.getElementById('container9')
 
 });
 function onShuffle() {
-    //Shuffling color based on randomy generated array
+
+    var arr = [tile11, tile22, tile33, tile44, tile55, tile66, tile77, tile88, tile99];
     var shuffledTileNo = randomArrayGenerator(arr);
-    document.getElementById('container' + shuffledTileNo[0]).style.backgroundColor = '#6F98A8';
-    document.getElementById('container' + shuffledTileNo[1]).style.backgroundColor = '#6F98A8';
-    document.getElementById('container' + shuffledTileNo[2]).style.backgroundColor = '#2B8EAD';
-    document.getElementById('container' + shuffledTileNo[3]).style.backgroundColor = '#2B8EAD';
-    document.getElementById('container' + shuffledTileNo[4]).style.backgroundColor = '#2F454E';
-    document.getElementById('container' + shuffledTileNo[5]).style.backgroundColor = '#2F454E';
-    document.getElementById('container' + shuffledTileNo[6]).style.backgroundColor = '#2F454E';
-    document.getElementById('container' + shuffledTileNo[7]).style.backgroundColor = '#BFBFBF';
-    document.getElementById('container' + shuffledTileNo[8]).style.backgroundColor = '#BFBFBF'
+    //mapping bacground colour to respetive object
+    container1.style.backgroundColor = shuffledTileNo[0].color;
+    container2.style.backgroundColor = shuffledTileNo[1].color;
+    container3.style.backgroundColor = shuffledTileNo[2].color;
+    container4.style.backgroundColor = shuffledTileNo[3].color;
+    container5.style.backgroundColor = shuffledTileNo[4].color;
+    container6.style.backgroundColor = shuffledTileNo[5].color;
+    container7.style.backgroundColor = shuffledTileNo[6].color;
+    container8.style.backgroundColor = shuffledTileNo[7].color;
+    container9.style.backgroundColor = shuffledTileNo[8].color;
 
     // setting tiles value as per randomly generated array
-    tile1.innerHTML = shuffledTileNo[0];
-    tile2.innerHTML = shuffledTileNo[1];
-    tile3.innerHTML = shuffledTileNo[2];
-    tile4.innerHTML = shuffledTileNo[3];
-    tile5.innerHTML = shuffledTileNo[4];
-    tile6.innerHTML = shuffledTileNo[5];
-    tile7.innerHTML = shuffledTileNo[6];
-    tile8.innerHTML = shuffledTileNo[7];
-    tile9.innerHTML = shuffledTileNo[8];
+    tile1.innerHTML = shuffledTileNo[0].id;
+    tile2.innerHTML = shuffledTileNo[1].id;
+    tile3.innerHTML = shuffledTileNo[2].id;
+    tile4.innerHTML = shuffledTileNo[3].id;
+    tile5.innerHTML = shuffledTileNo[4].id;
+    tile6.innerHTML = shuffledTileNo[5].id;
+    tile7.innerHTML = shuffledTileNo[6].id;
+    tile8.innerHTML = shuffledTileNo[7].id;
+    tile9.innerHTML = shuffledTileNo[8].id;
 }
 
 function onSort() {
-    var intialTileArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var intialTileArray = [tile11, tile22, tile33, tile44, tile55, tile66, tile77, tile88, tile99];
 
-    tile1.innerHTML = intialTileArray[0];
-    tile2.innerHTML = intialTileArray[1];
-    tile3.innerHTML = intialTileArray[2];
-    tile4.innerHTML = intialTileArray[3];
-    tile5.innerHTML = intialTileArray[4];
-    tile6.innerHTML = intialTileArray[5];
-    tile7.innerHTML = intialTileArray[6];
-    tile8.innerHTML = intialTileArray[7];
-    tile9.innerHTML = intialTileArray[8];
+    tile1.innerHTML = intialTileArray[0].id;
+    tile2.innerHTML = intialTileArray[1].id;
+    tile3.innerHTML = intialTileArray[2].id;
+    tile4.innerHTML = intialTileArray[3].id;
+    tile5.innerHTML = intialTileArray[4].id;
+    tile6.innerHTML = intialTileArray[5].id;
+    tile7.innerHTML = intialTileArray[6].id;
+    tile8.innerHTML = intialTileArray[7].id;
+    tile9.innerHTML = intialTileArray[8].id;
+
+    container1.style.backgroundColor = intialTileArray[0].color;
+    container2.style.backgroundColor = intialTileArray[1].color;
+    container3.style.backgroundColor = intialTileArray[2].color;
+    container4.style.backgroundColor = intialTileArray[3].color;
+    container5.style.backgroundColor = intialTileArray[4].color;
+    container6.style.backgroundColor = intialTileArray[5].color;
+    container7.style.backgroundColor = intialTileArray[6].color;
+    container8.style.backgroundColor = intialTileArray[7].color;
+    container9.style.backgroundColor = intialTileArray[8].color;
 }
 // Jumbles array
 function randomArrayGenerator(arr) {
